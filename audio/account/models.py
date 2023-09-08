e41cc0fd195dc23f7057add7f84ec48729e6cbbe
Rinzan-NP <nprinzan@gmail.com>
2023-09-08 19:16:10 +0530

    user datamodified and added profile model that extend the file

--- audio/account/models.py
+++ audio/account/models.py
@@ -1,3 +1,12 @@
 from django.db import models
-
+from base.models import BaseModel
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Profile(BaseModel):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
+    email_verified = models.BooleanField(default=False)
+    email_token = models.CharField(max_length=100, null = True, blank = True)
+    profile_image =models.ImageField(upload_to="profile",null=True)
+    phone_number = models.IntegerField()
+
--- audio/account/views.py
+++ audio/account/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth.models import User
+from . import views
+from django.http import HttpResponse
 
 # Create your views here.
+def register(request):
+
+    return render(request, 'accounts/register.html')
