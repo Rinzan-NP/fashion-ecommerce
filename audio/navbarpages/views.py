1d411a88d7e091043028151ed8f5bdfa8906c899
Rinzan-NP <nprinzan@gmail.com>
2023-10-05 12:32:58 +0530

    filtering wiht pagination

--- audio/navbarpages/views.py
+++ audio/navbarpages/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import redirect, render,render
 from products.models import Product,Product_image,Category,Size,Color,Brand,Wishlist,Cart,Profile,CartItems
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.db.models import Q
+from django.http import QueryDict
+
 # Create your views here.
 def home(request):
     context = {}
@@ -25,6 +27,9 @@ def shop_listing(request):
     category_filter = request.GET.get('category')
     sort_option = request.GET.get('sort')
 
+    # Create a QueryDict to store filter criteria for pagination links
+    filter_params = QueryDict(mutable=True)
+
     product_obj = Product.objects.filter(
         is_selling=True,
         category__unlisted=False,
@@ -36,27 +41,30 @@ def shop_listing(request):
     sizes = Size.objects.all()
     colors = Color.objects.all()
 
+    # Apply filters to the QuerySet and add them to the filter_params
     if category_filter:
-        product_obj = product_obj.filter(category = category_filter)
-
+        product_obj = product_obj.filter(category=category_filter)
+        filter_params['category'] = category_filter
 
     if query:
-   
         product_obj = product_obj.filter(
             Q(name__icontains=query) |
             Q(description__icontains=query)
         )
+        filter_params['q'] = query
 
     if brand_filter:
-       
         product_obj = product_obj.filter(brand=brand_filter)
+        filter_params['brand'] = brand_filter
 
     if sort_option == 'low_to_high':
         product_obj = product_obj.order_by('price')
+        filter_params['sort'] = 'low_to_high'
     elif sort_option == 'high_to_low':
         product_obj = product_obj.order_by('-price')
+        filter_params['sort'] = 'high_to_low'
 
-    paginator = Paginator(product_obj, 9)
+    paginator = Paginator(product_obj, 1)
     page = request.GET.get('page')
 
     try:
@@ -72,11 +80,15 @@ def shop_listing(request):
     context['colors'] = colors
     context['brands'] = brands
 
+    # Pass the filter_params to the template
+    context['filter_params'] = filter_params.urlencode()
+
     if request.user.is_authenticated and not request.user.is_staff:
         context['wishlist'] = [item.product for item in Wishlist.objects.filter(user=request.user.profile)]
         context['user'] = Profile.objects.get(user=request.user)
 
     return render(request, 'navbarpages/shop.html', context)
+
 
 
 def product_detail(request, uid):
